7612a1350a540c5899a9a40b564cd7f8780fbd2a
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-06-06 13:49:29 +0530

    init go server

--- src/docker-sample/main.go
+++ src/docker-sample/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main(){
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func main() {
+
+	myServerMux := http.NewServeMux()
+	myServerMux.HandleFunc("/submit", submitFn)
+	myServerMux.HandleFunc("/", helloworld)
+
+	server := http.Server{
+		Addr:    "8000",
+		Handler: myServerMux,
+	}
+
+}
+
+func submitFn(w http.ResponseWriter, r *http.Request) {
+	/* TODO */
+}
+
+func helloworld(w http.ResponseWriter, r *http.Request) {
+	/* TODO */
 }
